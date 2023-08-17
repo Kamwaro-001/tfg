7a7ae28f45bf57b245f89a11811c819e13d457c9
Kamwaro-001 <jobkamwaro9@gmail.com>
2023-08-18 02:32:52 +0300

    accounts app code cleanup

--- backend/server/apps/accounts/urls.py
+++ backend/server/apps/accounts/urls.py
@@ -1,15 +1,8 @@
 from django.urls import path, include
-from rest_framework import routers
 from .views import *
-
-router = routers.DefaultRouter()
-router.register("users", UserAPIView, basename="users")
 
 
 urlpatterns = [
     path("api/accounts/", include("djoser.urls")),
     path("api/accounts/", include("djoser.urls.jwt")),
-    # path("", include("dj_rest_auth.auth_token")),
-    # path('register/', include('dj_rest_auth.registration.urls')),
-    # path('', include(router.urls))
 ]
--- backend/server/apps/accounts/views.py
+++ backend/server/apps/accounts/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-
-# from rest_framework import generics
-# from rest_framework.mixins import *
 from rest_framework import viewsets
-# from rest_auth.registration.views import RegisterView
-from dj_rest_auth.registration.views import RegisterView
 from rest_framework.response import Response
 
 from .models import CustomUser
@@ -17,19 +12,11 @@ class CustomRegisterView(viewsets.ModelViewSet):
     serializer_class = UserRegisterSerializer
 
 
-# class UserAPIView(APIView):
-#     @staticmethod
-#     def get(request):
-#         users = CustomUser.objects.all()
-#         serializer = UserSerializer(users, many=True)
-#         return Response(serializer.data)
-      
 class UserAPIView(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = CustomUser.objects.all()
 
 
-# class GenericUserAPIView(generics.GenericAPIView, ListModelMixin, CreateModelMixin, UpdateModelMixin, Re)
 class GenericUserAPIView(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = CustomUser.objects.all()
